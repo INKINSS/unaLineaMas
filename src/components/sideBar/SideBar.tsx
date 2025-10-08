b20d6abebd44c0f479fc863651a3cefcd91ae1df
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-10-08 17:14:09 -0600

    feat: crea sidebar principal de vista articulos

---
+++ src/components/sideBar/SideBar.tsx
@@ -0,0 +1,49 @@
+import { Sidebar, SidebarContent, SidebarGroup, SidebarGroupContent, SidebarGroupLabel, SidebarProvider } from "../ui/sidebar";
+import { useEffect, useState } from "react";
+
+interface Props {
+    groupedArticles: Record<string, Array<{titleSideBar: string, slug: string}>>;
+    children: React.ReactNode;
+}
+
+const SideBar = ({ groupedArticles, children }: Props) => {
+
+    const [currentPath, setCurrentPath] = useState(window.location.pathname);
+
+    useEffect(() => {
+        setCurrentPath(window.location.pathname);
+    }, []);
+
+  return (
+    <SidebarProvider className="flex">
+    <Sidebar>
+        <SidebarContent className="p-6">
+            <SidebarGroup className="p-0">
+                <a href="/" className="text-gray font-medium text-sm px-0 no-underline">unaLineaMas</a>
+                <SidebarGroupContent>
+                    <SidebarGroupContent className="px-3">
+                        {Object.entries(groupedArticles).map(([category, articles]) => (
+                            <SidebarGroupContent key={category}>
+                                <SidebarGroupLabel className="text-gray font-medium text-sm px-0">{category}</SidebarGroupLabel>
+                                <SidebarGroupContent>
+                                    {articles.map((article) => (
+                                        <SidebarGroupContent key={article.slug}>
+                                            <a href={`/articles/${article.slug}`} className={currentPath === `/articles/${article.slug}` ? "text-gray text-sm px-0 underline font-normal" : "text-gray text-sm px-0 no-underline hover:underline font-normal"}>{article.titleSideBar}</a>
+                                        </SidebarGroupContent>
+                                    ))}
+                                </SidebarGroupContent>
+                            </SidebarGroupContent>
+                        ))}
+                    </SidebarGroupContent>
+                </SidebarGroupContent>
+            </SidebarGroup>
+        </SidebarContent>
+    </Sidebar>
+    <main className="mt-10 hide-scrollbar lg:px-5 pb-10">
+        {children}
+    </main>
+    </SidebarProvider>
+  );
+};
+
+export default SideBar;
